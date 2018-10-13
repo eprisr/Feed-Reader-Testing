45acebffff8165d8f85969fd599ce57877af30c9
Epris Harris-Burnett <erichardson@iamcreativeteam.com>
2018-10-13 13:43:21 -0400

    New Test Suite Inital Entries completes work

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,16 +80,27 @@ $(function() {
             expect(spy).toHaveBeenCalled();
             expect(body).toHaveClass('menu-hidden');
           });
-    });      
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Inital Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+            /* TODO: Write a test that ensures when the loadFeed
+             * function is called and completes its work, there is at least
+             * a single .entry element within the .feed container.
+             * Remember, loadFeed() is asynchronous so this test will require
+             * the use of Jasmine's beforeEach and asynchronous done() function.
+             */
+             beforeEach(function(done) {
+               loadFeed(0, done);
+             });
+
+             it('completes its work', function() {
+               const feed = document.querySelector('.feed');
+               const feedEntriesLength = feed.children.length;
+               expect(feedEntriesLength).toBeGreaterThan(0);
+             });
+        });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
